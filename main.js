ceff59dd905a71c133ff5249f057f222adf0f329
LashayC <cokleylashay@gmail.com>
2022-08-29 15:40:20 -0400

    added fetch with async/await

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const addButton = document.getElementById("addItem")
 const descriptionInput = document.getElementById("description")
 const destinationInput = document.getElementById("destination")
 const locationInput = document.getElementById("location")
-let photoInput
+let photo
 const wishList = document.getElementById("myWishlist")
 const h2OnList = document.querySelector("h2")
 const defaultImage = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTASyaCuEvNl3LRXjmwbhRiHbjBVKC-xH-CcQ&usqp=CAU'
@@ -11,76 +11,92 @@ const defaultImage = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTASy
 const authKey = 'nTmH_kB9w12-v2MpSH7uNeQiUHwVO0lU5Bs0qYM2Qn0'
 // https://api.unsplash.com/search/photos/?client_id=nTmH_kB9w12-v2MpSH7uNeQiUHwVO0lU5Bs0qYM2Qn0&query=paris&per_page=20
 
+
+
 async function getVacationImage(location, destination){
 
-    const response = await fetch(`https://api.unsplash.com/search/photos/?client_id=${authKey}&query=${location,destination}`, {
-        method: 'GET',
-        headers: {
-            'Accept-Version' : 'v1',
-            // 'Authorization' : authKey
-        }
-    })
-    let result = await response.json()
-    // console.log(result.results[0].links.html)
-    return photoInput = await result.results[0].links.html
-    // console.log(unsplashImage)
+    
 
-    // .then(result => {return result.json()})
-    // .then(data => {
-    //     // console.log(data)
-    //     // console.log('da link for paree',data.results[0].links.html)
-    // //    unsplashImage = data.results[0].links.html   
-    // //    return unsplashImage 
-    // data.results[0].links.html 
-    //     // console.log('da link for paree', unsplashImage)
-    // })
-    // // return console.log(unsplashImage)
+    try {
+        let response = await fetch(`https://api.unsplash.com/search/photos/?client_id=${authKey}&query=${location,destination}`)
+        let result = await response.json()
+        // console.log(result.results[0].links.html)
+        imageURL = result.results[0].urls.regular
+        console.log(imageURL)
+        return imageURL
+        // console.log(unsplashImage)
+        // .then(result => {return result.json()})
+        // .then(data => {
+        //     // console.log(data)
+        //     // console.log('da link for paree',data.results[0].links.html)
+        // //    unsplashImage = data.results[0].links.html   
+        // //    return unsplashImage 
+        // data.results[0].links.html 
+        //     // console.log('da link for paree', unsplashImage)
+        // })
+        // // return console.log(unsplashImage)
+        // makeWishList(photo, description, location, destination)
+    } catch (error) {
+            console.log(`error: ${error}`)
+    }
+    
+
 }
 
 
 
 
+
+
+// function getInput(e){
+//     e.preventDefault()
+//     let description = descriptionInput.value
+//     let location = locationInput.value
+//     let destination = destinationInput.value
+//     makeWishList(null, description, location, destination)
+
+// }
 
 //pull input from form
 //append to card parts
 //add bootstrap classes
-function makeWishList(e){
+async function makeWishList(e){
     e.preventDefault()
-    //get values from input
+    // get values from input
     let description = descriptionInput.value
     let location = locationInput.value
     let destination = destinationInput.value
-    console.log(photoInput)
+    // console.log(photoInput)
     // let thisPic = getVacationImage(location, destination)
 //    console.log('the callback func', thisPic.)
 
-function getVacationImage(location, destination){
+// function getVacationImage(location, destination){
 
-    fetch(`https://api.unsplash.com/search/photos/?client_id=${authKey}&query=${location,destination}`, {
-        method: 'GET',
-        headers: {
-            'Accept-Version' : 'v1',
-            // 'Authorization' : authKey
-        }
-    })
-    .then(result => {return result.json()})
-    .then(data => {
-    data.results[0].links.html 
-    })
-    .catch(error =>{
-        console.log(`error: ${error}`)
-    })
-}
-
-
+//     fetch(`https://api.unsplash.com/search/photos/?client_id=${authKey}&query=${location,destination}`, {
+//         method: 'GET',
+//         headers: {
+//             'Accept-Version' : 'v1',
+//             // 'Authorization' : authKey
+//         }
+//     })
+//     .then(result => {return result.json()})
+//     .then(data => {
+//      data.results[0].links.html 
+//     })
+//     .catch(error =>{
+//         console.log(`error: ${error}`)
+//     })
+// }
 
 
 
+photo = await getVacationImage(location, destination)
+
+console.log('this be it??', photo)
 
 
 
-
-    getVacationImage(location, destination)
+    // getVacationImage(location, destination)
     // create card w/classes
     const cardDiv = document.createElement('div')
     cardDiv.className = 'col-4'
@@ -120,8 +136,8 @@ function getVacationImage(location, destination){
     removeButton.setAttribute('id', 'removeButton')
 
     //add values to card
-    // photo == '' ? cardImage.src = defaultImage : cardImage.src = photo
-    cardImage.src = defaultImage
+    photo ? cardImage.src = photo : cardImage.src = defaultImage
+    // cardImage.src = defaultImage
     cardHeading.innerText = destination
     cardSubhead.innerText = location
     cardPara.innerText = description
